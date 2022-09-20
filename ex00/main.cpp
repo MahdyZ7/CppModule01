924e35e71afd5d623e70af543cae39997f9a275f
Ahmed Yassin <ayassin@lab3r4s12.42abudhabi.ae>
2022-09-20 13:16:01 +0400

    first ex

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 12:56:25 by ayassin           #+#    #+#             */
+/*   Updated: 2022/09/20 13:13:48 by ayassin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int main(void)
+{
+	std::cout <<  "Creating a Zombie called Ahmed\n";
+	Zombie ahmed("Ahmed");
+	ahmed.announce();
+	
+	// std::cout <<  "\nCreating a Zombie using newZombie\n";
+	// Zombie *altman = newZombie("Ole");
+	// altman->announce();
+	// delete altman;
+
+	// std::cout <<  "\nCreating a Zombie using randomChump\n";
+	// randomChump("Ole");
+}
---
+++ ex00/newZombie.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 12:08:23 by ayassin           #+#    #+#             */
+/*   Updated: 2022/09/20 13:01:11 by ayassin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name)
+{
+	Zombie *kenny = new Zombie(name);
+	return (kenny);
+}
