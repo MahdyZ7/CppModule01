91a64e1bdaba2496b793945d309737a89d3ddb11
MahdyZ7 <amahdy9@gmail.com>
2022-09-27 06:32:29 +0000

    global make file few errors in ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,46 +3,50 @@
 
 int main(int argc, char **argv)
 {
+	std::string		line;
+	std::string		outfile;
+	std::string		sub_out = argv[2];
+	std::string		sub_in = argv[3];
+	std::ifstream	fin;
+	std::ofstream 	fout;
+	char 			*char_arr;
+	// check for correct no of args
 	if (argc != 4)
 	{
 		std::cout << "wrong number of arguments\n";
 		return (0);
 	}
-	std::string line;
-	std::string outfile;
-	std::string sub = argv[2];
-	outfile.append(argv[1]);
-	outfile.append(".replace");
-	std::ifstream fin(argv[1]);
+	// open the read file
+	fin.open(argv[1]);
 	if (fin == 0)
 	{
 		std::cout << "The file " << argv[1] << " does not exist\n";
 		return (0);
 	}
-	char *char_arr = &outfile[0];
-	std::ofstream fout(char_arr);
-	std::getline(fin, line);
-	while (1)
+	// open the write file;
+	outfile.append(argv[1]);
+	outfile.append(".replace");
+	char_arr = &outfile[0];
+	fout.open(char_arr);
+	if (fout == 0)
 	{
-		for (int pos = 0; argv[2][0] &&line.find(argv[2], pos) < line.length(); ++pos)
+		fin.close();
+		std::cout << argv[1] << ": Permistion denied\n";
+		return (0);
+	}
+	// loop to read and write
+	while (getline(fin, line))
+	{
+		for (int pos = 0; sub_out[0] && line.find(sub_out, pos) < line.length();)
 		{
 			pos = line.find(argv[2], pos);
-			line = line.substr(0, pos) + argv[3] + line.substr(pos + sub.length(), -1);
-			pos = (line.substr(0, pos) + argv[3]).length();
+			line = line.substr(0, pos) + sub_in +
+				line.substr(pos + sub_out.length(), -1);
+			pos = (line.substr(0, pos) + sub_in).length();
 		}
 		fout << line;
-		if (line.find("//n", 0) < line.length())
+		if (fin.eof() != 1)
 			fout << "\n";
-		// for(int i = 0 ; line[i] ; ++i)
-		// 	std::cout << line[i] << " ";
-		// std::cout << std::endl;
-		if (std::getline(fin, line))
-		{
-			if (fin.eof() != 1)
-				fout << "\n";
-		}
-		else
-			break;
 	}
 	fin.close();
 	fout.close();
---
+++ ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl a;
+	a.complain("ERROR");
+	a.complain("DEBUG");
+	a.complain("INFO");
+	a.complain("WARNING");
+	a.complain("DRINK");
+}
